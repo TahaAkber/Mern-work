d5728d824d4efef64cc7f5a34d2f4038a6f94ecd
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-29 19:18:17 +0500

    successfully created post request
    Here we completed get and post requests

--- beginner-Mern/server/index.js
+++ beginner-Mern/server/index.js
@@ -6,7 +6,8 @@ const Usermodel = require("./models/Users");
 mongoose.connect(
   "mongodb+srv://Tahaakber:lumia123@cluster0.vnsu7wc.mongodb.net/Merntutorial?retryWrites=true&w=majority"
 );
-
+//since we posting data to backend with jsonfile we need to use express that takes data its a builtin feature of express
+app.use(express.json());
 app.get("/getuser", async (req, res) => {
   try {
     const result = await Usermodel.find({});
@@ -16,7 +17,15 @@ app.get("/getuser", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
+//here we pass an object that we need to insert into database
+app.post("/createuser", async (req, res) => {
+  const user = req.body;
+  //we initialize usermodel to store user into database
+  const newuser = new Usermodel(user);
+  await newuser.save();
+  //showing data to frontend using res.json();
+  res.json(user);
+});
 app.listen(3001, () => {
   console.log(`Server is running on port ${3001}`);
 });
