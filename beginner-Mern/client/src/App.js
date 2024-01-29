627cf8752034a9dcc00fcb11c5b24a1e7cc08818
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-30 00:22:01 +0500

    now we hit to the end point to get data from backend to front end

--- beginner-Mern/client/src/App.js
+++ beginner-Mern/client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [users, setusers] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getuser").then((response) => {
+      setusers(response.data);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="data">
+        {users.map((i) => {
+          return (
+            <div>
+              <h1>{i.name}</h1>
+              <h3>{i.age}</h3>
+              <h6>{i.username}</h6>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
