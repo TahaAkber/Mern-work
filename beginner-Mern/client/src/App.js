967d468599c16326a85fb26f528b5cdb3229def7
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-30 02:13:50 +0500

    Completed post and get request and i have also handled it from front end using react

--- beginner-Mern/client/src/App.js
+++ beginner-Mern/client/src/App.js
@@ -4,11 +4,25 @@ import Axios from "axios";
 
 function App() {
   const [users, setusers] = useState([]);
+  const [name, setname] = useState("");
+  const [age, setage] = useState(0);
+  const [username, setusername] = useState("");
+
   useEffect(() => {
     Axios.get("http://localhost:3001/getuser").then((response) => {
       setusers(response.data);
     });
   }, []);
+  const addtobackend = async () => {
+    await Axios.post("http://localhost:3001/createuser", {
+      name: name,
+      age: age,
+      username: username,
+    }).then((response) => {
+      setusers([...users, { name: name, age: age, username: username }]);
+      alert("UserCreated");
+    });
+  };
   return (
     <div className="App">
       <div className="data">
@@ -21,6 +35,37 @@ function App() {
             </div>
           );
         })}
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          placeholder="Enter Name"
+          onChange={(e) => {
+            setname(e.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          onChange={(e) => {
+            setage(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Enter username"
+          onChange={(e) => {
+            setusername(e.target.value);
+          }}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            addtobackend();
+          }}
+        >
+          Click to Create
+        </button>
       </div>
     </div>
   );
