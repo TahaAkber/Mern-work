0ad4f89efbd6dae49bebdec7575eb663e59b3147
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-30 23:34:37 +0500

    app.put function is added
    multiple types Of apps.get is used which is getting all object and other one is getting object 1 by 1

--- beginner-Mern/server/index.js
+++ beginner-Mern/server/index.js
@@ -12,7 +12,7 @@ mongoose.connect(
 //since we posting data to backend with jsonfile we need to use express that takes data its a builtin feature of express
 app.use(express.json());
 app.use(cors());
-app.get("/getuser", async (req, res) => {
+app.get("/user", async (req, res) => {
   try {
     const result = await Usermodel.find({});
     res.json(result);
@@ -21,8 +21,31 @@ app.get("/getuser", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+app.get("/user/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    // Usermodel.findById(userId)
+    const result = await Usermodel.find({ _id: userId });
+    res.json(result);
+  } catch (err) {
+    console.error("Error retrieving users:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/user/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const body = req.body;
+    const result = await Usermodel.updateOne({ _id: userId }, body);
+    res.json(result);
+  } catch (err) {
+    console.error("Error retrieving users:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 //here we pass an object that we need to insert into database
-app.post("/createuser", async (req, res) => {
+app.post("/user", async (req, res) => {
   const user = req.body;
   //we initialize usermodel to store user into database
   const newuser = new Usermodel(user);
