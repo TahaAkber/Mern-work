7c53cf15384f347e49c803e6c474d67010fe791a
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-03-11 22:50:16 +0500

    Typography learned

--- Material Ui/material-demo/src/App.tsx
+++ Material Ui/material-demo/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Multitypography from "./components/Multitypography";
 
 function App() {
   return (
     <div className="App">
+      <Multitypography />
     </div>
   );
 }
---
+++ Material Ui/material-demo/src/components/Multitypography.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Typography } from "@mui/material";
+const Multitypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3">Heading 3</Typography>
+      <Typography variant="h4">Heading 4</Typography>
+
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos similique
+        hic dolore libero dicta quod alias vero maiores ad sit beatae, officia
+        ipsum ullam iusto aliquam veritatis iure odio rerum nisi magnam iste
+        harum. Aspernatur quasi ex odit at a atque laboriosam modi! Dolorem,
+        porro.
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ducimus
+        quis quas eum quae nemo ratione dolorem voluptas, doloribus obcaecati
+        vero at quaerat cupiditate inventore eius unde, excepturi enim eveniet
+        nisi molestias autem! Deleniti, minima! Cum, repellat quia iusto
+        consequuntur facere ipsum libero? Quo, corrupti!
+      </Typography>
+    </div>
+  );
+};
+
+export default Multitypography;
