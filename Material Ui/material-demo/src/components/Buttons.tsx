40cec268c42aa02e52bafff6a23e64fa14b80ada
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-03-11 23:42:36 +0500

    buttons and multiple variant of buttons

--- Material Ui/material-demo/src/components/Buttons.tsx
+++ Material Ui/material-demo/src/components/Buttons.tsx
@@ -3,10 +3,32 @@ import { Button } from "@mui/material";
 import Stack from "@mui/material/Stack";
 const Buttons = () => {
   return (
-    <Stack spacing={10} direction="row-reverse">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">contained</Button>
-      <Button variant="outlined">outlined</Button>
+    <Stack spacing={4}>
+      <Stack spacing={10} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">contained</Button>
+        <Button variant="outlined">outlined</Button>
+      </Stack>
+      <Stack spacing={4} direction="row" sx={{ margin: "25px" }}>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
     </Stack>
   );
 };
