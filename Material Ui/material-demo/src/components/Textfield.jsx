7a05a178be598a227e1eeee44d827ce73a0b1887
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-03-13 23:56:32 +0500

    ADDED inputadornment

--- Material Ui/material-demo/src/components/Textfield.jsx
+++ Material Ui/material-demo/src/components/Textfield.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Stack, TextField } from "@mui/material";
+import { Stack, TextField, InputAdornment } from "@mui/material";
 const Textarea = () => {
   return (
     <Stack spacing={2}>
       <Stack direction="row" spacing={4}>
-        <TextField label="Name" variant="filled" />
-        <TextField label="Name" variant="outlined" />
-        <TextField label="Name"  variant="standard"/>
+        <TextField label="Name" variant="filled" color="secondary" />
+        <TextField label="Name" variant="outlined" color="primary" />
+        <TextField label="Name" variant="standard" color="success" />
+      </Stack>
+      <Stack spacing={4}>
+        <TextField
+          label="Amount"
+          inputProps={{
+            StartAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="Weight"
+          inputProps={{
+            endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+          }}
+        />
       </Stack>
     </Stack>
   );
