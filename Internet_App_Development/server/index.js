310667bd16c7e73f9e6490c52fb6bf769f3d746c
Taha <taha.akber007@gmail.com>
2024-05-16 00:10:18 +0500

    Update index.js

--- Internet_App_Development/server/index.js
+++ Internet_App_Development/server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const mongodb = require("mongodb+srv://tahaakber:lumia123@cluster0.o5sdrdh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+const cors = require("cors");
+
 mongoose.connect(
-  "mongodb+srv://tahaakber:lumia123@cluster0.o5sdrdh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://tahaakber:lumia123@cluster0.o5sdrdh.mongodb.net/Userentries?retryWrites=true&w=majority&appName=Cluster0"
 );
 app.use(express.json());
 app.use(cors());
@@ -17,6 +18,6 @@ app.get("/user", async (res, req) => {
     res.sta;
   }
 });
-app.listen(5000, () => {
-  console.log("server is running at port ", 5000);
+app.listen(3001, () => {
+  console.log("server is running at port ", 3001);
 });
