f59dd274ae08a80f4bf6f4a6bb5cf577dc61e1d3
Taha <taha.akber007@gmail.com>
2024-05-17 23:52:01 +0500

    Update index.js

--- Internet_App_Development/serverlocal/index.js
+++ Internet_App_Development/serverlocal/index.js
@@ -1,6 +1,13 @@
-import http from "http";
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "Text/plain");
-})
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "Text/plain");
+  res.write("helloworld");
+  res.end();
+});
+const port = 3000;
+
+server.listen(port, (req, res) => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
