d3178525abe6e4a8e0e5274e4d7b82580ba71c65
Taha <taha.akber007@gmail.com>
2024-05-17 23:56:34 +0500

    Created http server with routes

--- Internet_App_Development/serverlocal/index.js
+++ Internet_App_Development/serverlocal/index.js
@@ -1,13 +1,23 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "Text/plain");
-  res.write("helloworld");
-  res.end();
-});
-const port = 3000;
+  const reqUrl = url.parse(req.url).pathname;
 
+  if (reqUrl == "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "Text/plain");
+    res.write("helloworld");
+    res.end();
+  } else if (reqUrl == "/home") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "Text/plain");
+    res.write("I love you from home");
+    res.end();
+  }
+});
+
+const port = 3000;
 server.listen(port, (req, res) => {
   console.log(`Server running at http://localhost:${port}/`);
 });
