394bf93b21534dbabaaba4d6048087070232a3f3
Taha <taha.akber007@gmail.com>
2024-05-17 23:58:48 +0500

    Update index.js

--- Internet_App_Development/serverlocal/index.js
+++ Internet_App_Development/serverlocal/index.js
@@ -14,6 +14,11 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "Text/plain");
     res.write("I love you from home");
     res.end();
+  } else if (reqUrl == "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "Text/plain");
+    res.write("About us");
+    res.end();
   }
 });
 
