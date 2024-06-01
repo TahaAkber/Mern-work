f449b49972d82f244d96d4596adff43c99887ced
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-06-01 21:05:04 +0500

    practicing javascript

---
+++ Internet_App_Development/finalpractice/javscript/Es6+.js
@@ -0,0 +1,34 @@
+// console.log(arrFucn(10));
+
+// let x;
+// function arrFucn(x) {
+//     return x;
+// }
+
+//teemplate literals
+
+// let x = 4;
+// let y = 5;
+// console.log("Sum of numbers is", x, "and", y, "is", +(x + y));
+// console.log(`sum of ${x} and ${y} is ${x + y}`);
+// console.log("The sum of " + x + " and " + y + " is " + (x + y));
+
+//Object destructuring
+
+// let car = {
+//   name: "honda",
+//   model: "2008",
+//   Engine: "Vx8",
+// };
+
+// function cardetails(car) {
+//   console.log(car.name, car.model, car.Engine);
+// }
+
+// function cardetails2({ name, model, Engine }) {
+//   console.log(name, model, Engine);
+// }
+// cardetails(car);
+// cardetails2(car);
+
+
